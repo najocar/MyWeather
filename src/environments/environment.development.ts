90b511d952999b50dd7839d9f58475ad58f46387
najocar <josecarloscastrolopez@gmail.com>
2023-11-23 09:00:06 +0100

    update api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     apis:{
         weather: {
-            url: 'http://api.weatherapi.com/v1/forecast.json?days=3&aqi=no&alerts=no',
+            url: 'https://api.weatherapi.com/v1/forecast.json?days=3&aqi=no&alerts=no',
             key: 'ccc21e7614184d6d9e7113705231411',
             testPosition: '37.667927, -4.724776'
         }
